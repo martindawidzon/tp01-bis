525b55e7581409c7e3ac760d490bc08280dabb27
MARTIN DAWIDZON <50031557@ort>
2026-04-28 08:09:27 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp01_bis_dawidzon_naiman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
